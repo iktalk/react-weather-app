a01d60387108c95143175bd6711eebd9ff230084
iktalk <iktalk.94@gmail.com>
2022-04-26 22:51:12 -0400

    added footer component

---
+++ src/Footer.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <div className="Footer">
+      <p>
+        <a
+          href="https://github.com/iktalk/react-weather-app.git"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Open-Source Code by Ishmeet Kaur
+        </a>
+      </p>
+    </div>
+  );
+}
